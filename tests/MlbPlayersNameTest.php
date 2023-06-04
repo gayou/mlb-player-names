26abee9b969d09d4a87c0746007edecd1d31372e
gayou <gayou.ura@gmail.com>
2023-06-04 14:31:39 +0900

    引退した選手は名前を取得できないテストを追加、他。

--- tests/MlbPlayersNameTest.php
+++ tests/MlbPlayersNameTest.php
@@ -1,6 +1,7 @@
 <?php namespace Gayou\MlbPlayersName\Test;
 
 use PHPUnit\Framework\TestCase;
+use Gayou\MlbPlayersName\CsvDataCreator;
 use Gayou\MlbPlayersName\MlbPlayersName;
 
 class MlbPlayersNameTest extends TestCase
@@ -8,16 +9,27 @@ class MlbPlayersNameTest extends TestCase
 
     public static function setUpBeforeClass(): void
     {
+        CsvDataCreator::setup();
         MlbPlayersName::init();
     }
 
 
     public function testJapanize()
     {
-        // ホセ・アブレイユ
-        $this->assertSame(MlbPlayersName::japanize('Jose Abreu'), 'ホセ・アブレイユ');
+        // マイク・トラウト
+        $this->assertSame(MlbPlayersName::japanize('Mike Trout'), 'マイク・トラウト');
 
+        // 大谷翔平
+        $this->assertSame(MlbPlayersName::japanize('Shohei Ohtani'), '大谷翔平');
+    }
+
+    /**
+     * 引退した選手は名前を取得できない
+     * 
+     */
+    public function testRetirePlayer()
+    {
         // 松井秀喜
-        $this->assertSame(MlbPlayersName::japanize('Hideki Matsui'), '松井秀喜');
+        $this->assertSame(MlbPlayersName::japanize('Hideki Matsui'), '');
     }
 }
