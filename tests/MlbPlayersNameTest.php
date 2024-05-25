8a1c4384f883b0873874442783fd097719efb758
gayou <gayou.ura@gmail.com>
2024-05-25 14:04:15 +0900

    アクセント記号付きの名前の取得に対応

--- tests/MlbPlayersNameTest.php
+++ tests/MlbPlayersNameTest.php
@@ -27,12 +27,34 @@ class MlbPlayersNameTest extends TestCase
     }
 
     /**
+     * 現役選手の名前を取得できること（正規化した名前）
+     * 
+     */
+    public function testJapanizeNormalize()
+    {
+        // Rafael Marchán -> ラファエル・マルチャン
+        $this->assertSame(MlbPlayersName::japanize('Rafael Marchán'), 'ラファエル・マルチャン');
+
+        // Yohan Ramírez -> ヨハン・ラミレス
+        $this->assertSame(MlbPlayersName::japanize('Yohan Ramírez'), 'ヨハン・ラミレス');
+        
+        // José Berríos -> ホセ・ベリオス
+        $this->assertSame(MlbPlayersName::japanize('José Berríos'), 'ホセ・ベリオス');
+
+        // Reynaldo López -> レイナルド・ロペス
+        $this->assertSame(MlbPlayersName::japanize('Reynaldo López'), 'レイナルド・ロペス');
+
+        // Nasim Nuñez -> ナシム・ヌニェス
+        $this->assertSame(MlbPlayersName::japanize('Nasim Nuñez'), 'ナシム・ヌニェス');
+    }
+
+    /**
      * 引退した選手は名前を取得できない
      * 
      */
     public function testRetirePlayer()
     {
         // 松井秀喜
-        $this->assertSame(MlbPlayersName::japanize('Hideki Matsui'), '');
+        $this->assertSame(MlbPlayersName::japanize('Hideki Matsui'), null);
     }
 }
